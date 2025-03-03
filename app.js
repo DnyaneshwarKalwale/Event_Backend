a1992951a1a76a5268a2df20c2b2c399472ea2eb
Dnyaneshwar Balaji Kalwale <prembkalwale@gmail.com>
2025-03-03 23:08:46 +0530

    Backend

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import bodyParser from "body-parser";
-import router from "./routes/index";
+import router from "./routes/index.js";
 
 
 const app = express();
